f8e70a9485a9929423a3a1d872585b2649c40857
Javier Zarate <zarate.javier.jz@gmail.com>
2022-04-26 21:12:31 -0600

    single page recipe complete. Fetched recipe by ID

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,42 @@
 // styles
 import './Recipe.css';
 
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
 
 function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/');
+      }, 2000);
+    }
+  }, [error, history]);
+
+  return (
+    <div className="recipe">
+      {isPending && <div className="error">Loading...</div>}
+      {error && <div className="loading">{error}</div>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Recipe;
