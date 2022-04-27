b95cf55867cce8eab29b29eccb9325295d63f0a3
Javier Zarate <zarate.javier.jz@gmail.com>
2022-04-26 20:13:37 -0600

    fetched data and printing data into cards

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,21 @@
+import { useFetch } from '../../hooks/useFetch';
+
 // styles
 import './Home.css';
 
 import React from 'react';
+import RecipeList from '../../components/RecipeList';
 
 function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Home;
